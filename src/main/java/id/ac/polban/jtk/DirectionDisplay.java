ba78c31d911072e3d9cdf318ea47e0f2ebcc7c39
adevaraz <zaraveda.zv@gmail.com>
2020-04-10 17:37:09 +0700

    create direction display class

---
+++ src/main/java/id/ac/polban/jtk/DirectionDisplay.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polban.jtk;
+
+/**
+ *
+ * @author Zara Veda
+ */
+public class DirectionDisplay {
+    private static boolean status; //true = up; false = down
+    
+    void setStatus(boolean status) {
+        this.status = status;
+    }
+}
