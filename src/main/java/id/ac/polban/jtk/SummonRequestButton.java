fd1ea26c35fa8944d94a81ccde6df9f312f1911d
RUP <roma.ulina.tif418@polban.ac.id>
2020-04-11 16:49:12 +0700

    Create SummonRequestButton
    create method TurnLightOff()
    create method Released()

--- src/main/java/id/ac/polban/jtk/SummonRequestButton.java
+++ src/main/java/id/ac/polban/jtk/SummonRequestButton.java
@@ -11,5 +11,18 @@ package id.ac.polban.jtk;
  * @author RomaUlina
  */
 public class SummonRequestButton {
+    boolean status = true; //true = pressed/turnlight summon button & false = released/turnoff summon button
+ 
+    void TurnLightOff(){
+    }
     
+    void Released( int floorNumber, boolean direction){
+        //CabNavigator
+        if(true/*a cab was already stopped at the floor that the summon button was 
+        released from AND the doors of the cab are open [floorNumber==floor that cab Is On]*/){
+            //TurnLightOff()
+            //start timer in Door Timer 3 second;
+            //DoorOpeningDevice.CloseDoors()
+        }
+    }
 }
