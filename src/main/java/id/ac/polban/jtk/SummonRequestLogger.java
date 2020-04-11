7f6b3d79a8c26a5d0892b547c52722802f11b358
andhika276 <andhikazaini276@gmail.com>
2020-04-11 13:39:11 +0700

    summon button

---
+++ src/main/java/id/ac/polban/jtk/SummonRequestLogger.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polban.jtk;
+import java.util.concurrent.LinkedBlockingQueue;
+/**
+ *
+ * @author Asus
+ */
+ class SummonRequestLogger implements Runnable {
+    private int floorNumber;
+    private ElevatorController elevatorController;
+    private LinkedBlockingQueue<FloorRequestSignal> signals;
+    @Override
+    public void run() {
+        
+        
+    }
+    
+    
+}
