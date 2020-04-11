93c38c44f02882c8d6a14359a4481dfb92557421
andhika276 <andhikazaini276@gmail.com>
2020-04-11 09:36:48 +0700

    commit

--- src/main/java/id/ac/polban/jtk/App.java
+++ src/main/java/id/ac/polban/jtk/App.java
@@ -2,10 +2,16 @@ package id.ac.polban.jtk;
 
 public final class App {
     /**
+<<<<<<< HEAD
      * @throws InterruptedException
      * 
      */
     public static void main(String[] args) throws InterruptedException {
+=======
+     * 
+     */
+    public static void main(String[] args) {
+>>>>>>> commit
         ElevatorController
             .getInstance()
             .run();
---
+++ src/main/java/id/ac/polban/jtk/FloorRequestSignal.java
@@ -0,0 +1,49 @@
+package id.ac.polban.jtk;
+
+public class FloorRequestSignal {
+    /**
+     * 
+     */
+    interface Response {
+        void onComplete();
+    }    
+
+    /**
+     * 
+     */
+    private final int cabID;
+
+    /**
+     * 
+     */
+    private final int floorNumber;
+
+    /**
+     * 
+     */
+    private final Response response;
+
+    public FloorRequestSignal(int cabID, int floorNumber, Response response) {
+        this.cabID = cabID;
+        
+        this.floorNumber = floorNumber;
+
+        this.response = response;
+    }
+
+    public void process () {
+        // if (this.elevatorController.cabController.cabs[cabID].cabNavigator.floorNumber == floorNumber) {
+        //     // Send a signal to the cab’s door opening device to
+        //     // open the doors
+        // }
+        // else {
+        //     this.elevatorController.cabController.cabs[cabID].floorRequestButton[floorNumber].turnLightOn();
+        //     requests.add(this.elevatorController.new FloorRequest(cabID, floorNumber));
+
+        //     // Add floor request to queue
+        //     requests.drainTo(this.elevatorController.queue);
+        // }
+
+        response.onComplete();
+    }
+}
