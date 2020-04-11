7b314f63349ccadf1f9bc9c6deaace65c3e96533
adevaraz <zaraveda.zv@gmail.com>
2020-04-11 16:15:36 +0700

    adding template

---
+++ src/main/java/id/ac/polban/jtk/DoorOpeningDevice.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polban.jtk;
+
+/**
+ *
+ * @author Zara Veda
+ */
+public class DoorOpeningDevice {
+    
+    /**
+     * penanggung jawab: Nadhilah 
+     * @param cabID
+     * pintu elevator sudah terbuka, method doorsOpened
+     * menjalankan timer untuk menutup pintu elevator secara otomatis
+     */
+    void doorsOpened(int cabID) {
+        
+    }
+    
+    /**
+     * penanggung jawab: Nadhilah 
+     * pintu belum terbuka, method openDoors membuka pintu elevator
+     */
+    void openDoors() {
+        
+    }
+}
