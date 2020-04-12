d98f5777563912709faa29b9584b1c29baf4dadc
RUP <roma.ulina.tif418@polban.ac.id>
2020-04-12 15:22:15 +0700

    Update SummonRequestButton.java

--- src/main/java/id/ac/polban/jtk/SummonRequestButton.java
+++ src/main/java/id/ac/polban/jtk/SummonRequestButton.java
@@ -4,18 +4,22 @@
  * and open the template in the editor.
  */
 package id.ac.polban.jtk;
-
+import id.ac.polban.jtk.ElevatorEngine.Direction;
 /**
  *
  * @author Andhika
  * @author RomaUlina
  */
+enum LightStatus {
+    OFF, ON;
+}
 public class SummonRequestButton {
-      enum LightStatus {
-          ON,OF;
-    }
       
-     private LightStatus statuslight;
+    private LightStatus statuslight;
+    boolean status; //true = pressed/turnlight summon button & false = released/turnoff summon button
+    LightStatus DirectionUp, DirectionDown;
+    Cab cab;
+    
        public void turnLightOn() {
         this.statuslight = LightStatus.ON;
     }
@@ -23,11 +27,21 @@ public class SummonRequestButton {
      * 
      */
     public void turnLightOff() {
-        this.statuslight = LightStatus.ON;.OFF;
+        this.statuslight = LightStatus.ON;
     }
-      
-      
-    static void Pressed(enum direction, int floorNumber) {
+    
+    
+    void TurnLightOff(ElevatorEngine.Direction direction){
+        if(direction == ElevatorEngine.Direction.UP){
+            DirectionUp = LightStatus.OFF;
+            System.out.println("The summon button light is " + DirectionUp);
+        }else if(direction == ElevatorEngine.Direction.DOWN){
+            DirectionDown = LightStatus.OFF;
+            System.out.println("The summon button light is " + DirectionDown);
+        }
+    }
+    
+    //static void Pressed(enum direction, int floorNumber) {
         // ElevatorController.getInstance().getSummonRequestLogger().sendSignal(cabID, floorNumber, new ISignalResponse(){
         //     @Override
         //     public void onComplete() {
@@ -35,21 +49,29 @@ public class SummonRequestButton {
         //     }
         // } );
         // released(direction,floorNumber);
-    }
-    
-    static void Released(enum direction, int floorNumber) {
-                    //CabNavigator
-        //if(true a cab was already stopped at the floor that the summon button was 
-        //released from AND the doors of the cab are open [floorNumber==floor that cab Is On]){
-            //TurnLightOff()
-            //start timer in Door Timer 3 second;
+    //}
+
+    void Released(int floorNumber, ElevatorEngine.Direction direction){
+        int floorNumberCab = cab.getCabNavigator().getFloorNumber();
+        if(floorNumber==floorNumberCab /*&& the door cab is open*/){
+            TurnLightOff(direction);
+            DoorTimer doorTimer = new DoorTimer();
+            doorTimer.StartTimer();
             //DoorOpeningDevice.CloseDoors()
-        //}
-        // ElevatorController.getInstance().getSummonRequestLogger().sendSignal(cabID, floorNumber, new ISignalResponse(){
-        //     @Override
-        //     public void onComplete() {
-        //         // TODO Auto-generated method stub
-        //     }
-        // } );
+        }
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
