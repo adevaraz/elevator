682604d36fa70462ab98fcba8283ee6dc73b0ec0
ichacahyawulan <ichacahyawulan@gmail.com>
2020-04-14 16:02:12 +0700

    Process pressed signal from floor request button (incomplete-update)

--- src/main/java/id/ac/polban/jtk/ElevatorEngine.java
+++ src/main/java/id/ac/polban/jtk/ElevatorEngine.java
@@ -1,6 +1,9 @@
 package id.ac.polban.jtk;
 
-public class ElevatorEngine {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ElevatorEngine extends TimerTask{
     public enum Direction {
         UP(1),
         DOWN(-1);
@@ -18,13 +21,54 @@ public class ElevatorEngine {
             return value;
         }
     };
+    
+    /**
+     * Posisi ketinggian dari elevator dengan asumsi elevator berada di 
+     * ketinggian 0m saat awal beroperasi
+     */
+    private double height = 0.0;
 
     /**
+     * Kecepatan pergerakan elevator dengan asumsi elevator tidak bergerak
+     * (bergerak dengan kecepatan 0) saat awal beroperasi
+     */
+    private double speed = 0;
+
+    /**
+     * Arah pergerakan elevator
+     */
+    private Direction direction = Direction.UP;
+    
+    public static int i = 0;
+    public void run(){
+        ++i;
+    }
+    
+    /**
      * 
-     * @param speed     the speed in m/s.
+     */
+    public ElevatorEngine() {
+        // Buat timer yang akan dapat mengubah variable `height` dalam setiap
+        // detiknya tergantung speed dan direction
+        Timer timer = new Timer();
+        TimerTask task = new ElevatorEngine(); 
+          
+        timer.schedule(task, 1000, 1000); 
+    }
+
+    /**
+     * Mengubah kecepatan dan arah elevator
+     * @param speed     the speed in m/s
      * @param direction 
      */
-    void move(double speed, Direction direction) {
-
+    public void move(double speed, Direction direction) {
+        // ubah variable height dan direction
+        this.height = i * speed;
+        this.direction = direction;
     }
+
+    public double getHeight() {
+        return height;
+    }
+
 }
