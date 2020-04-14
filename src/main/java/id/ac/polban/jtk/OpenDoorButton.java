d2d7c7aa475c5ea78e1b1429e60eabd21f0ebba4
adevaraz <zaraveda.zv@gmail.com>
2020-04-14 12:39:45 +0700

    modify comment

--- src/main/java/id/ac/polban/jtk/OpenDoorButton.java
+++ src/main/java/id/ac/polban/jtk/OpenDoorButton.java
@@ -50,17 +50,17 @@ public class OpenDoorButton {
      * penanggung jawab: Zara Veda
      */
     void released() {
-        /* dikomen karena error, stuck belum bisa dapetin cabID*/
-//        if(ElevatorController.getCabController.isAvailable(cabID) && 
-//            status){
+        // TODO: If elevator stopped at floor x and the cab doors opened
+        if(true){
             Status status = Status.RELEASED;
             setStatus(status);
-//            Start Timer
+            
+            // Start Timer
 //            DoorTimer timer = new DoorTimer();
 //            timer.StartTimer();
 //            TimeUnit.SECONDS.sleep(1);
 //            timer.StopTimer();
-//        }
+        }
         
         //Close the door
         operator.doorClosed();
