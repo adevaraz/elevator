4611262cb4b4082f30a68818ecfe09805fd89cc3
andhika276 <andhikazaini276@gmail.com>
2020-04-14 19:01:39 +0700

    remove

--- src/main/java/id/ac/polban/jtk/FloorRequestButton.java
+++
@@ -1,7 +0,0 @@
-package id.ac.polban.jtk;
-
-public interface FloorRequestButton {
-    public void turnLightOn();
-    
-    public void turnLightOff();
-    }
--- src/main/java/id/ac/polban/jtk/FloorRequestLogger.java
+++
@@ -1,5 +0,0 @@
-package id.ac.polban.jtk;
-
-public interface FloorRequestLogger {
-    public void pressed(int cabID, int floorNumber);
-    }
